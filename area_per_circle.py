171654515e0fc239dabaa213338d4e77eb281d3f
Lloyd Najac <lloyd.najac@stu.ocsb.ca>
2022-09-27 06:54:52 +0000

    Added area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# Created By: Lloyd Najac
+# Date: Sep. 27, 2022
+# This program calculates the area and perimeter of a circle with a radius of 15mm.
+
+
+def main():
+    import math
+
+    rad = 15
+    circ = math.pi * 2 * rad
+    area = math.pi * rad**2
+    print(circ, area)
+    math.sqrt(100)
+    math.sqrt(25)
+
+
+if __name__ == "__main__":
+    main()
